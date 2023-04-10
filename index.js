aa039c817cca8ab4d26e7466cd83be4131171f1c
VITA-R9 <jhj9144@gmail.com>
2023-04-10 23:31:51 +0900

    socket message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 let http = require('http');
 const cors = require('cors');
 const app = express();
+const mysql = require('mysql');
 const port = process.env.PORT || 5000;
 let server = http.createServer();
 let io = require('socket.io')(server, {
@@ -13,11 +14,28 @@ let io = require('socket.io')(server, {
 // middleware
 app.use(express.json());
 app.use(cors());
+const clients = {};
 io.on('connection', (socket) => {
     console.log('socket connected');
     console.log(socket.id, 'has joined');
+
     socket.on('/test', (message) => {
         console.log(message);
+    });
+
+    socket.on('sign-in', (id) => {
+        console.log(id);
+        clients[id] = socket;
+        console.log(clients);
+    });
+
+    socket.on("message", (msg) => {
+        console.log(msg);
+        const targetId = msg.targetId;
+        console.log('targetId : ' + msg.targetId);
+        if (clients[targetId]) {
+            clients[targetId].emit("message", msg);
+        }
     });
 });
 
